c8a73882d7ecf0132af7ab4780053609dc4393c0
thebestgirlever <maya1999@mail.ru>
2025-02-07 12:54:43 +0300

    added icon of loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import Api from "./api/Api";
 import SearchBar from "./components/SearchBar";
 import Card from "./components/Card";
+import LoadingIconn from "./assets/loading.svg";
 
 const App = () => {
     const [username, setUsername] = useState("");
@@ -34,13 +35,22 @@ const App = () => {
             <h1>Найдите свой репозиторий</h1>
             <SearchBar onSearch={handleSearch} />
             {error && <p>{error}</p>}
-            {loading && <p>Loading...</p>}
-            <div className={styles.container}>
-                {repos.slice(0, 10).map((repo) => (  // Ограничиваем до 10 карточек
-                    <Card key={repo.id} repo={repo} />
-                ))}
-            </div>
+
+            {loading ? (
+                <img src={LoadingIconn} className={styles.loading} alt="loading" />
+            ) : (
+                <div className={styles.container}>
+                    {repos.length > 0 ? (
+                        repos.slice(0, 30).map((repo) => (
+                            <Card key={repo.id} repo={repo} />
+                        ))
+                    ) : (
+                        <p>Не найдено, такой человек точно существует?</p>
+                    )}
+                </div>
+            )}
         </div>
+
     );
 };
 
